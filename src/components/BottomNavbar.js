dca8e63312037d92596040ddf574315f6910792c
bhanu_helius <bhanuprakash18m@gmail.com>
2024-05-19 23:16:12 +0530

    increasing bottom nav bar height

--- src/components/BottomNavbar.js
+++ src/components/BottomNavbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BottomNavbar = () => {
   return (
-    <nav class="navbar navbar-dark bg-dark fixed-bottom bottom-nav d-md-none d-flex justify-content-around" style={{height : '5vh'}}>
+    <nav class="navbar navbar-dark bg-dark fixed-bottom bottom-nav d-md-none d-flex justify-content-around" style={{height : '6vh'}}>
         <a class="nav-link text-light" href="#">
             <i class="bi bi-house"></i>
         </a>
